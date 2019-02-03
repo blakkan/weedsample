3ba43940fbe472f3987b76c28c83a610f1b816c4
John Blakkan <john1@blakkan.net>
2019-02-03 08:52:58 -0800

    Added a few more control pages

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,13 @@
 class DashboardController < ApplicationController
 
-  def index_screen
+  def display_main_screen
   end
+
+  def display_select_weed_screen
+  end
+
+  def display_statistics_screen
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "dashboard#index_screen"
+  root to: "dashboard#display_main_screen"
+  get 'display_main_screen' => 'dashboard#display_main_screen'
+  get 'display_select_weed_screen' => 'dashboard#display_select_weed_screen'
+  get 'display_statistics_screen' => 'dashboard#display_statistics_screen'
+
 end
