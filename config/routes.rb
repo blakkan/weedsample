21a1ca5c9e11b819fc27d9da9b14ff72bbef4369
John Blakkan <john.blakkan@spirent.com>
2019-02-06 16:25:02 -0800

    Add routes to support new controller methods

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,10 @@ Rails.application.routes.draw do
   get 'return_data/:start_date/:end_date' => 'dashboard#return_data'
   get 'return_sample_data/:start_date/:end_date(.:format)' => 'dashboard#return_sample_data'
 
+  get 'display_upload_screen' => 'dashboard#display_upload_screen'
+  get 'display_simulator' => 'dashboard#display_simulator'
+  put 'upload_data' => 'dashboard#upload_data'
+  get 'reset_data' => 'dashboard#reset_data'
+  get 'completely_clear_data' => 'dashboard#completely_clear_data'
+
 end
